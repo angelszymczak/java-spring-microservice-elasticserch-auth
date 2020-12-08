9b9aa2c1193fe28308d22e8013e1d95a3af8a715
Angel Szymczak <angel.szymczak@southworks.com>
2020-12-08 10:57:20 -0300

    Project setup

---
+++ src/main/java/com/magneto/scanner/MutantDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.magneto.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magneto/scanner/MutantDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magneto.scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
