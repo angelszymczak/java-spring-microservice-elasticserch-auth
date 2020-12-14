b6f1f6d1741946aef4f21af28b127afcd082bb24
Angel Szymczak <angel.szymczak@southworks.com>
2020-12-14 02:04:47 -0300

    Add ElasticSearch layer for GeneticFactor repository

--- src/main/java/com/magneto/scanner/repository/GeneticFactorRepository.java
+++ src/main/java/com/magneto/scanner/repository/GeneticFactorRepository.java
@@ -5,6 +5,15 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 import java.util.List;
 
+/*
+ *
+ * TODO
+ *  Research about query for string arrays field types based on accuracy,
+ *  currently we added a workaround to trait a DNA chain like a single String.
+ *  So, it like an ID, should be unique to detect which patterns is present
+ *  over specific combination, but if we would scanner a new pattern probably
+ *  you'll parse it splitting by quotes "," since It to get dimensions of genetic code.
+ */
 public interface GeneticFactorRepository extends ElasticsearchRepository<GeneticFactorDocument, String> {
-    List<GeneticFactorDocument> findByDna(String[] dna);
+    List<GeneticFactorDocument> findByDna(String dna);
 }
