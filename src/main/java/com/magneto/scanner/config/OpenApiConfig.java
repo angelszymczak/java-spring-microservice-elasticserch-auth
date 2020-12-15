8d32acaa04e87a37b472ffbea001104fc3463f2f
Angel Szymczak <angel.szymczak@southworks.com>
2020-12-15 01:42:36 -0300

    Add version restriction to V1 API endpoints access

--- src/main/java/com/magneto/scanner/config/OpenApiConfig.java
+++ src/main/java/com/magneto/scanner/config/OpenApiConfig.java
@@ -11,32 +11,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    private static final String BASE_PACKAGE_URL = "com.magneto.scanner.controllers";
-    private static final String VERSION = "v1";
-
     @Bean
     public OpenAPI customOpenAPI(
             @Value("${application-title}") String appTitle,
-            @Value("${application-contact-email}") String appContactEmail,
-            @Value("${application-contact-name}") String appContactName,
+            @Value("${application-version}") String appVersion,
             @Value("${application-description}") String appDescription,
             @Value("${application-terms}") String appTerms,
             @Value("${application-license-name}") String appLicenseName,
             @Value("${application-license-url}") String appLicenseUrl,
-            @Value("${application-version}") String appVersion
+            @Value("${application-contact-name}") String appContactName,
+            @Value("${application-contact-email}") String appContactEmail
     ) {
         return new OpenAPI()
                 .info(new Info()
                         .title(appTitle)
-                        .contact(new Contact()
-                                .email(appContactEmail)
-                                .name(appContactName))
                         .version(appVersion)
                         .description(appDescription)
                         .termsOfService(appTerms)
                         .license(new License()
                                 .name(appLicenseName)
                                 .url(appLicenseUrl))
+                        .contact(new Contact()
+                                .name(appContactName)
+                                .email(appContactEmail))
                 );
     }
 }
